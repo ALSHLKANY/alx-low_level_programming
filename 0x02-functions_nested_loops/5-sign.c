ef5f998a3cc39f5104f9ef8f522f83fa0b1b57c1
ALSHLKANY <moalshlkany@gmail.com>
2023-02-21 17:34:26 +0200

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,14 +9,14 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+");
+		_putchar("+");
 		return (1);
 	}
 	else if (n < 0)
 	{
-		printf("0");
+		_putchar("0");
 		return (0);
 	}
-	printf("-1");
+	_putchar("-1");
 	return (-1);
 }
