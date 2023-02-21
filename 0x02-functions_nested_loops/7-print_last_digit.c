61e7f119645780685c1b748ab64105efd51b8ce8
ALSHLKANY <moalshlkany@gmail.com>
2023-02-21 18:53:51 +0200

    fix INT_MIN result

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,9 +7,11 @@
  */
 int print_last_digit(int n)
 {
-	if (n < 0)
-		n *= -1;
+	int mod = n % 10;
 
-	_putchar((n % 10) + '0');
-	return (n % 10);
+	if (mod < 0)
+		mod *= -1;
+
+	_putchar(mod + '0');
+	return (mod);
 }
