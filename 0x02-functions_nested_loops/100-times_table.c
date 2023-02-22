c4afbf831267b6d9a5a112099383b31d054951ff
ALSHLKANY <moalshlkany@gmail.com>
2023-02-22 04:12:25 +0200

    fix betty style

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,43 +9,32 @@ void print_times_table(int n)
 {
 	if (n <= 15 && n >= 0)
 	{
-		int i, j;
+		int i, j, res;
 
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
-				int res = i * j;
-
-				if (j != 0)
-				{
-					if (res < 10)
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-					}
-					else if (res < 100)
-					{
-						_putchar(' ');
-						_putchar(' ');
-					}
-					else
-					{
-						_putchar(' ');
-					}
-				}
-				if (res < 10)
+				res = i * j;
+				if (j == 0)
+					_putchar('0');
+				else if (res < 10)
 				{
 					_putchar(res + '0');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
 				}
 				else if (res < 100)
 				{
+					_putchar(' ');
+					_putchar(' ');
 					_putchar(res / 10 + '0');
 					_putchar(res % 10 + '0');
 				}
 				else
 				{
+					_putchar(' ');
 					_putchar(res / 100 + '0');
 					res %= 100;
 					_putchar(res / 10 + '0');
