61bc69fdbcaef6de7c68baf95d88bc30dfbe88fb
ALSHLKANY <moalshlkany@gmail.com>
2023-02-27 10:24:42 +0200

    task 8 try to // simple array with wrong lengt

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,6 +8,8 @@ void print_array(int *a, int n)
 {
 	int i;
 
+	if (n <= 0)
+		printf("\n");
 	for (i = 0; i < n; i++)
 	{
 		if (i == n - 1)
@@ -15,4 +17,5 @@ void print_array(int *a, int n)
 		else
 			printf("%d, ", a[i]);
 	}
+
 }
