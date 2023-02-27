58607630084db9ca38b00da9fc505bd514b08406
ALSHLKANY <moalshlkany@gmail.com>
2023-02-27 08:58:37 +0200

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_rev - print string in reverse
+ * @s : string
+ */
+void print_rev(char *s)
+{
+	int len = _strlen(s);
+	int i;
+
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
