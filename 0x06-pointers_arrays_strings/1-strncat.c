0549898f6ce614f8271ebe482cb3f7ce069844ee
ALSHLKANY <moalshlkany@gmail.com>
2023-03-01 19:08:32 +0200

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,7 +13,7 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (i = 0; dest[i] != '\0'; i++)
 	{}
-	for (j = 0; j < n; j++, i++)
+	for (j = 0; j < n && src[j] != '\0'; j++, i++)
 		dest[i] = src[j];
 	return (dest);
 }
