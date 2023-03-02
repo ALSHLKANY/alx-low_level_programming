fd9c568b6b1b4e5c97d865cbe5ed3f4a8b0d3b1e
ALSHLKANY <moalshlkany@gmail.com>
2023-03-02 07:29:46 +0200

    7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * leet - encodes a string into 1337.
+ * @s: string
+ * Return: string
+ */
+char *leet(char *s)
+{
+	char l33t[] = "a4A4e3E3o0O0t7T7l1L1";
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; l33t[j] != '\0'; j++)
+		{
+			if (s[i] == l33t[j])
+			{
+				s[i] = l33t[j + 1];
+				break;
+			}
+		}
+	}
+	return (s);
+}
