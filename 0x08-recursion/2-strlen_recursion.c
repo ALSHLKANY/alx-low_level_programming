0e46565b5eaf79735a4dfc6b4d5897094defd0e7
ALSHLKANY <moalshlkany@gmail.com>
2023-03-08 10:00:05 +0200

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - return the length of s
+ * @s: string
+ * Return: length of s
+ */
+int _strlen_recursion(char *s)
+{
+	int x = 0;
+
+	if (*s != '\0')
+	{
+		x = _strlen_recursion(s + 1);
+		return (x++);
+	}
+	return (0);
+}
