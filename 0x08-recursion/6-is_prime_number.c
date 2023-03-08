dad3cb37516ebc58267f5f1d2a31fab290a2a3dc
ALSHLKANY <moalshlkany@gmail.com>
2023-03-08 17:48:08 +0200

    task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * solve - :)
+ * @n: number
+ * @i: var :(
+ * Return: 1 || 0
+ */
+int solve(int n, int i)
+{
+	if (i >= n || n == 2)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	solve(n, ++i);
+}
+/**
+ * is_prime_number - :)
+ * @n: number
+ * Return: 1 || 0
+ */
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (0);
+	return (solve(n, 2));
+}
