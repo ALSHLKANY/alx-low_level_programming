214601532f9db1359cb1b5c4036190015bdd1cfa
ALSHLKANY <moalshlkany@gmail.com>
2023-03-12 22:40:49 +0200

    5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,37 +10,38 @@
  */
 int main(int argc, char *argv[])
 {
-	int x = atoi(argv[1]);
+	int x;
+	int coins;
+	(void)x;
+	(void)coins;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	if else(strchr(argv[1], '-'))
+	if (strchr(argv[argc - 1], '-'))
 	{
 		printf("0\n");
 		return (1);
 	}
-	else
-	{
-		int coins = 0;
+	x = atoi(argv[argc - 1]);
+	coins = 0;
 
-		while (x > 0)
-		{
-			if (x % 25 == 0)
-				x -= 25;
-			else if (x % 10 == 0)
-				x -= 10;
-			else if (x % 5 == 0)
-				x -= 5;
-			else if (x % 2 == 0)
-				x -= 2;
-			else
-				x--;
-			coins++;
-		}
-		printf("%d\n", coins);
+	while (x > 0)
+	{
+		if (x % 25 == 0)
+			x -= 25;
+		else if (x % 10 == 0)
+			x -= 10;
+		else if (x % 5 == 0)
+			x -= 5;
+		else if (x % 2 == 0)
+			x -= 2;
+		else
+			x--;
+		coins++;
 	}
+	printf("%d\n", coins);
 	return (0);
 }
