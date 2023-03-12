4fda3201e71a3d7be77c8e2c19f065191f819ea7
ALSHLKANY <moalshlkany@gmail.com>
2023-03-12 21:08:36 +0200

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main
+ * @argc: number of arguments
+ * @argv: vector of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
