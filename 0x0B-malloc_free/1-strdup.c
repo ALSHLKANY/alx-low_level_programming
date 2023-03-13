76b52162f2a6253d8d0acac805a1a6c2343ef2d3
ALSHLKANY <moalshlkany@gmail.com>
2023-03-13 10:17:46 +0200

    1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - copy string
+ * @str: string
+ * Return: pointer to the copt of str
+ */
+char *_strdup(char *str)
+{
+	int size = 0, i;
+	char *t;
+
+	if (str == "\0")
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		size++;
+	t = malloc(sizeof(char) * size + 1);
+	if (t == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		t[i] = str[i];
+	return (t);	
+}
